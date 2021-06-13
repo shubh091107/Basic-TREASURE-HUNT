85da086df3200bf051e0fc7292ae1ee5ea8d94d3
shubh091107 <73987682+shubh091107@users.noreply.github.com>
2021-06-13 10:03:14 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("lightblue")
+    text("Hint: Perform a particular task !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A T A D E S B A", 100,300)
+    fill("lightblue")
+    text("Hint: Stores all the information !!", 100,320)
+}
